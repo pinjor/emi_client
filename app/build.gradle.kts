32892c46ee7b7d0ca89ce5f6c483564c70627039
MotiurRahmanSany <mdmotiurrahmansany@gmail.com>
2025-10-27 17:56:46 +0600

    changed version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.emilockerclient"
         minSdk = 30
         targetSdk = 36
-        versionCode = 4
-        versionName = "4.0"
+        versionCode = 5
+        versionName = "5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
