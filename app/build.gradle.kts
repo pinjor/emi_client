3292dafc030cbab6a6f15925592fac0c6c485985
MotiurRahmanSany <mdmotiurrahmansany@gmail.com>
2025-10-30 18:58:21 +0600

    updated app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.emilockerclient"
         minSdk = 30
         targetSdk = 36
-        versionCode = 4
-        versionName = "4.0"
+        versionCode = 5
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
